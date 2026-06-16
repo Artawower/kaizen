0fb8e8ddb63292d5c0506595f0fe11470b4731e6
Artur Iaroshenko <artawower@protonmail.com>
2026-06-16 20:06:33 +0300

    fix(nix): support Intel macOS home-manager

--- dotfiles/dot_config/nix/flake.nix
+++ dotfiles/dot_config/nix/flake.nix
@@ -2,21 +2,35 @@
   description = "Nix configuration";
 
   inputs = {
-    nixpkgs.url        = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home-manager.url   = "github:nix-community/home-manager/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nix-darwin.url     = "github:LnL7/nix-darwin/master";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    zen-browser.url    = "github:youwen5/zen-browser-flake";
-    dms.url            = "github:AvengeMedia/DankMaterialShell";
-    noctalia.url       = "github:noctalia-dev/noctalia-shell";
+    zen-browser.url = "github:youwen5/zen-browser-flake";
+    dms.url = "github:AvengeMedia/DankMaterialShell";
+    noctalia.url = "github:noctalia-dev/noctalia-shell";
   };
 
-  outputs = { self, nix-darwin, nixpkgs, home-manager, dms, ... }@inputs:
+  outputs =
+    {
+      self,
+      nix-darwin,
+      nixpkgs,
+      home-manager,
+      dms,
+      ...
+    }@inputs:
     let
       user = import ./user.nix;
+      darwinSystem =
+        if builtins ? currentSystem && nixpkgs.lib.hasSuffix "-darwin" builtins.currentSystem then
+          builtins.currentSystem
+        else
+          "aarch64-darwin";
 
-      mkDarwinHome = system: extraModules:
+      mkDarwinHome =
+        system: extraModules:
         home-manager.lib.homeManagerConfiguration {
           pkgs = import nixpkgs {
             inherit system;
@@ -24,10 +38,12 @@
           };
           modules = [
             { _module.args = { inherit user; }; }
-          ] ++ extraModules;
+          ]
+          ++ extraModules;
         };
 
-      mkLinuxHome = system: extraModules:
+      mkLinuxHome =
+        system: extraModules:
         home-manager.lib.homeManagerConfiguration {
           pkgs = import nixpkgs {
             inherit system;
@@ -35,26 +51,26 @@
               allowUnfree = true;
               # Obsidian pins an EOL electron that nixpkgs marks insecure. Allow it
               # explicitly (version-resilient, unlike permittedInsecurePackages).
-              allowInsecurePredicate = pkg:
-                builtins.elem (nixpkgs.lib.getName pkg) [ "electron" ];
+              allowInsecurePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [ "electron" ];
             };
           };
           extraSpecialArgs = { inherit inputs; };
           modules = [
             { _module.args = { inherit user; }; }
-          ] ++ extraModules;
+          ]
+          ++ extraModules;
         };
     in
     {
       darwinConfigurations.${user.hostname} = nix-darwin.lib.darwinSystem {
-        specialArgs = { inherit self user; };
+        specialArgs = { inherit self user darwinSystem; };
         modules = [
           ./darwin.nix
         ];
       };
 
       homeConfigurations = {
-        "${user.username}@mac" = mkDarwinHome "aarch64-darwin" [
+        "${user.username}@mac" = mkDarwinHome darwinSystem [
           ./hosts/mac.nix
         ];
 
